660ed6a452d2d48aa56e48ae04ab127dc0b56535
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-03-19 18:27:12 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -1,31 +1,39 @@
 package test;
-public class Auto {
-	   String modelo;
-	   int precio;
-	   Asiento[] asientos;
-	   String marca;
-	   Motor motor;
-	   int registro; 
-	   static int contidadCreados;
 
-	   int cantidadAsientos(){
-		   int total = 0;
-	       for (Asiento i; this.asientos){
-	           if(i != null) {
-	        	   total++;
-	           }
-	       }
-	       return total;
-	    	}
-	   String verificarIntegridad() {
-		   for (Asiento u;this.asientos) {
-			 if (u != null && (u.registro != this.registro)) {
-				 return "Las piezas no son originales";
-			 }
-		   }
-		   if (this.motor.registro != this.registro) {
-			   return "Las piezas no son originales";
-		   }
-		   return "Auto original";
-	}
+public class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantindadCreados;
+
+    int cantidadAsientos(){
+        int total = 0;
+
+        for (Asiento i:this.asientos){
+            if (i != null){
+                total++;
+            }
+        }
+        return total;
+    }
+    String verificarIntegridad(){
+        for (Asiento u:this.asientos){
+        	if(u != null && (u.registro != this.registro)){
+        		return "Las piezas no son originales";
+        		
+        	}
+        	
+        }
+        if (this.motor.registro != this.registro) {
+        	return "Las piezas no son originales";
+        }
+        return "Auto original";
+    }
+
+
+    
+
 }
