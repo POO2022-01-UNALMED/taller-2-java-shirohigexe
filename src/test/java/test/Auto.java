4e907d569acda139fb42c8d11f5c9c902ae37eb5
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-03-19 18:00:44 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,31 @@
+package test;
+public class Auto {
+	   String modelo;
+	   int precio;
+	   Asiento[] asientos;
+	   String marca;
+	   Motor motor;
+	   int registro; 
+	   static int contidadCreados;
+
+	   int cantidadAsientos(){
+		   int total = 0;
+	       for (Asiento i; this.asientos){
+	           if(i != null) {
+	        	   total++;
+	           }
+	       }
+	       return total;
+	    	}
+	   String verificarIntegridad() {
+		   for (Asiento u;this.asientos;u++) {
+			 if (this.registro != u.registro && this.registro != this.motor.registro) {
+				 return "Las piezas no son originales";
+				 
+			 }
+			 else {
+				 return "Auto original";
+			 }
+		   }
+	}
+}
