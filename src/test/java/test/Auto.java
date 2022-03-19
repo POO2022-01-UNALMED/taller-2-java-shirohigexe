9d99e262d0dfcffdfd27e1942d34725713485ec1
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-03-19 18:16:06 -0500

    Update Auto.java

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -18,13 +18,15 @@ public class Auto {
 	       return total;
 	    	}
 	   String verificarIntegridad() {
-		   for (Asiento u;this.asientos;u++) {
-			 if (this.registro != u.registro && this.registro != this.motor.registro) {
-				 return "Las piezas no son originales";
+		   for (Asiento u;this.asientos) {
+			 if (u != null) {
+				 if (this.registro != u.registro && this.registro != this.motor.registro) {
+					 return "Las piezas no son originales";
+				 }
+				 else {
+					 return "Auto original";
+				 }
 				 
-			 }
-			 else {
-				 return "Auto original";
 			 }
 		   }
 	}
